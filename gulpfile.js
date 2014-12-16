f58d1c23733517239ad2de351cb225ece4426127
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-12-16 16:56:23 +0900

    Hello gulp-tvm-tsc v0.1

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var tsc = require('./');
+
+gulp.task('tsc', function () {
+  return gulp.src('test/*.ts')
+    .pipe(tsc({
+      version: '1.0.1', // default: latest
+      option: '--target ES5 --removeComments'
+    })).on('error', gutil.log)
+    .pipe(gulp.dest('test/'))
+});
