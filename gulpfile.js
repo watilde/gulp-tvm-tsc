17ca9d22c98148c315d95017a3c4a5c6359173b1
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-12-19 18:09:35 +0900

    Add gulp test task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var tsc = require('./');
 
-gulp.task('tsc', function () {
+gulp.task('test', function () {
   return gulp.src('test/*.ts')
     .pipe(tsc({
       version: '1.0.1', // default: latest
